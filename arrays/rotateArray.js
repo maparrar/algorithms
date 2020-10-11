d36b26c780035def0fbf8dc442924339b0e78676
maparrar <maparrar@gmail.com>
2020-10-11 13:20:08 -0500

    Rotate array: one line solution

---
+++ arrays/rotateArray.js
@@ -0,0 +1,12 @@
+// Source: https://leetcode.com/problems/rotate-array/
+
+const rotate = function(nums, k) {
+    nums.unshift(...nums.splice(-(k % nums.length), k % nums.length));
+};
+
+const array = [1,2,3,1];
+rotate([1,2,3,1], 3);
+console.log(array);
+rotate([1,2,3,1], 6);
+console.log(array);
+
