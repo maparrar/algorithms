5c907434fc5fabe93a976d969d40f656b08e03bd
Miguel Parra <miguel.parra@gmail.com>
2024-05-30 15:07:11 -0500

    Fibonacci solve strategies

--- src/5.recursion_dynamic/fibonacci.test.ts
+++ src/5.recursion_dynamic/fibonacci.test.ts
@@ -1,12 +1,41 @@
-import { fibonacciRecursion } from './fibonacci'
+import { fibonacciDynamic, fibonacciDynamicRecursive, fibonacciFormula, fibonacciRecursive } from './fibonacci'
 import { measure } from '../tools/performance'
 
 describe('test cases Recursion and Dynamic', () => {
-  it('Fibonacci: recursion', () => {
-    expect(fibonacciRecursion(5)).toEqual(5)
-    expect(fibonacciRecursion(10)).toEqual(55)
-    expect(fibonacciRecursion(20)).toEqual(6765)
+  it('should solve Fibonacci with recursion', () => {
+    expect(fibonacciRecursive(5)).toEqual(5)
+    expect(fibonacciRecursive(10)).toEqual(55)
+    expect(fibonacciRecursive(20)).toEqual(6765)
 
-    measure(fibonacciRecursion, 20)
+    measure(fibonacciRecursive, 20)
+  })
+
+  it('should solve Fibonacci with the formula (aprox)', () => {
+    expect(fibonacciFormula(5)).toEqual(5)
+    expect(fibonacciFormula(10)).toEqual(55)
+    expect(fibonacciFormula(20)).toEqual(6765)
+    expect(fibonacciFormula(70)).toEqual(190392490709135)
+
+    measure(fibonacciFormula, 200)
+  })
+
+  it('should solve Fibonacci with dynamic programming with recursion', () => {
+    const fibonacci = fibonacciDynamicRecursive()
+    expect(fibonacci(5)).toEqual(5)
+    expect(fibonacci(10)).toEqual(55)
+    expect(fibonacci(20)).toEqual(6765)
+    expect(fibonacci(70)).toEqual(190392490709135)
+
+    measure(fibonacci, 200)
+  })
+
+  it('should solve Fibonacci with dynamic programming', () => {
+    const fibonacci = fibonacciDynamic()
+    expect(fibonacci(5)).toEqual(5)
+    expect(fibonacci(10)).toEqual(55)
+    expect(fibonacci(20)).toEqual(6765)
+    expect(fibonacci(70)).toEqual(190392490709135)
+
+    measure(fibonacci, 200)
   })
 })
--- src/5.recursion_dynamic/fibonacci.ts
+++ src/5.recursion_dynamic/fibonacci.ts
@@ -1,5 +1,40 @@
+export const fibonacciRecursive = (n: number): number => {
+  if (n <= 2 ) {
+    return 1
+  } else {
+    return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2)
+  }
+}
 
-export const fibonacciRecursion = (n: number): number => {
-  
-  return -1 - n
+export const fibonacciFormula = (n: number): number => {
+  const sqrt5 = Math.sqrt(5)
+  const phi = (1 + sqrt5) / 2
+  return Math.round((Math.pow(phi, n) - Math.pow((Math.pow(-phi, -1)), n)) / sqrt5)
+}
+
+export const fibonacciDynamicRecursive = (): (n: number) => number => {
+  const cached: number[] = []
+
+  return function fibo (n: number) {
+    if (cached[n]) {
+      return cached[n]
+    } else if (n <= 2 ) {
+      return 1
+    } else {
+      cached[n] = fibo(n - 1) + fibo(n - 2)
+      return cached[n]
+    }
+  }
+}
+
+export const fibonacciDynamic = (): (n: number) => number => {
+  const cached: number[] = [0, 1]
+
+  return (n: number) => {
+    for (let i = 2; i <= n; i++) {
+      cached[i] = cached[i - 1] + cached[i - 2]
+    }
+
+    return cached[n]
+  }
 }
